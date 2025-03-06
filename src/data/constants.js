ab12e1ca58d290b11b0c68dfa313d1e784e0dc31
vineethpradeep <vineethpadeeps@gmail.com>
2025-03-06 00:28:27 +0000

    update bio skills

--- src/data/constants.js
+++ src/data/constants.js
@@ -41,13 +41,13 @@ import { RiAiGenerate } from "react-icons/ri";
 export const bio = {
   name: "Vineeth Pradeep",
   roles: [
+    "UI / UX Designer",
+    "UI / UX Developer",
     "Frontend Developer",
     "Application Developer",
-    "UI Developer",
     "MERN Stack Developer",
     "MEAN Stack Developer",
     "Mobile App Developer",
-    "UI / UX Designer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
